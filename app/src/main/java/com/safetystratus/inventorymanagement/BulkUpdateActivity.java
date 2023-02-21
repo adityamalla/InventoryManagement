add6ea879258f27417d2710677573d10492c9289
adityamalla <aditya.m.v.s.s@gmail.com>
2023-02-22 00:05:08 +0530

    RFID bulk update

--- app/src/main/java/com/safetystratus/inventorymanagement/BulkUpdateActivity.java
+++ app/src/main/java/com/safetystratus/inventorymanagement/BulkUpdateActivity.java
@@ -2,12 +2,18 @@ package com.safetystratus.inventorymanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
@@ -25,16 +31,22 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import com.google.android.gms.common.util.Hex;
+import com.zebra.rfid.api3.TagData;
+
 import net.sqlcipher.database.SQLiteDatabase;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
-public class BulkUpdateActivity extends AppCompatActivity {
+public class BulkUpdateActivity extends AppCompatActivity implements RFIDHandlerBulkUpdate.ResponseHandlerInterface {
     public static final String PASS_PHRASE = DatabaseConstants.PASS_PHRASE;
     boolean connected = false;
     String loggedinUsername = "";
@@ -52,10 +64,13 @@ public class BulkUpdateActivity extends AppCompatActivity {
     EditText enteredCodeValue;
     ListView codeList;
     Button addtoList;
+    ProgressBar spinner;
     CustomizedListViewBulkUpdate adapter;
     //generate list
     ArrayList<String> codestobeaddedtolist = new ArrayList<String>();
+    ArrayList<String> newList = new ArrayList<String>();
     ProgressDialog progressSynStart = null;
+    RFIDHandlerBulkUpdate rfidHandler;
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,16 +127,23 @@ public class BulkUpdateActivity extends AppCompatActivity {
         Log.e("selecteduserid1>>",selectedUserId+"**");
         loggedinUserSiteId = intent.getStringExtra("site_id");
         md5Pwd = intent.getStringExtra("md5pwd");
-        rfid = findViewById(R.id.rfidbtn);
-        barcode = findViewById(R.id.barcodebtn);
+        //rfid = findViewById(R.id.rfidbtn);
+        //barcode = findViewById(R.id.barcodebtn);
         codeLabel = findViewById(R.id.codeLabel);
         codeList = findViewById(R.id.codeList);
         enteredCodeValue = findViewById(R.id.enteredCodeValue);
         addtoList = findViewById(R.id.addCodeToList);
-        rfid.setChecked(true);
+        spinner = (ProgressBar)findViewById(R.id.progressBarBulkUpdate);
+//        rfid.setChecked(true);
+        rfidHandler = new RFIDHandlerBulkUpdate();
+        rfidHandler.onCreate(this);
         codeLabel.setText("Scan or enter RFID code to edit container details");
         enteredCodeValue.setHint("Enter RFID code");
-        rfid.setOnClickListener(new View.OnClickListener() {
+        /*IntentFilter filter = new IntentFilter();
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        filter.addAction(getResources().getString(R.string.activity_intent_filter_action));
+        registerReceiver(myBroadcastReceiver, filter);*/
+        /*rfid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(rfid.isChecked()){
@@ -138,13 +160,10 @@ public class BulkUpdateActivity extends AppCompatActivity {
                     enteredCodeValue.setHint("Enter Barcode details");
                 }
             }
-        });
+        });*/
         addtoList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String rfid = "1";
-                if(barcode.isChecked())
-                    rfid = "0";
                 codestobeaddedtolist.add(enteredCodeValue.getText().toString());
                 //instantiate custom adapter
                 CustomizedListViewBulkUpdate adapter = new CustomizedListViewBulkUpdate(codestobeaddedtolist, BulkUpdateActivity.this);
@@ -166,6 +185,8 @@ public class BulkUpdateActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
+            //unregisterReceiver(myBroadcastReceiver);
+            rfidHandler.onDestroy();
             final Intent myIntent = new Intent(BulkUpdateActivity.this,
                     HomeActivity.class);
             myIntent.putExtra("user_id", selectedUserId);
@@ -181,4 +202,143 @@ public class BulkUpdateActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        //unregisterReceiver(myBroadcastReceiver);
+        rfidHandler.onDestroy();
+    }
+    /*private BroadcastReceiver myBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Bundle b = intent.getExtras();
+            //  This is useful for debugging to verify the format of received intents from DataWedge
+            //for (String key : b.keySet())
+            //{
+            //    Log.v(LOG_TAG, key);
+            //}
+            if (action.equals(getResources().getString(R.string.activity_intent_filter_action))) {
+                //  Received a barcode scan
+                try {
+                    displayScanResult(intent, "via Broadcast");
+                } catch (Exception e) {
+                    //  Catch if the UI does not exist when we receive the broadcast... this is not designed to be a production app
+                }
+            }
+        }
+    };
+
+    private void displayScanResult(Intent initiatingIntent, String howDataReceived)
+    {
+        String decodedSource = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_source));
+        String decodedData = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data));
+        String decodedLabelType = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_label_type));
+
+        if (null == decodedSource)
+        {
+            decodedSource = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_source_legacy));
+            decodedData = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_data_legacy));
+            decodedLabelType = initiatingIntent.getStringExtra(getResources().getString(R.string.datawedge_intent_key_label_type_legacy));
+        }
+        Log.e("TestDecodeData>>",decodedData+"---");
+    }*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        rfidHandler.onPause();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        rfidHandler.onResume();
+    }
+
+    @Override
+    public void handleTagdata(TagData[] tagData) {
+        final StringBuilder sb = new StringBuilder();
+        final int[] scanCounts = {0};
+        for (int index = 0; index < tagData.length; index++) {
+            byte[] bytes = Hex.stringToBytes(String.valueOf(tagData[index].getTagID().toCharArray()));
+            try {
+                sb.append(new String(bytes, "UTF-8") + "&&&");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        runOnUiThread(new Runnable() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void run() {
+                String[] tagList = sb.toString().split("&&&");
+                ArrayList<String> list = new ArrayList<String>();
+                for (int u=0;u<tagList.length;u++){
+                    list.add(tagList[u]);
+                }
+                // Create a new ArrayList
+
+                // Traverse through the first list
+                for (String element : list) {
+
+                    // If this element is not present in newList
+                    // then add it
+                    if (!newList.contains(element)) {
+
+                        newList.add(element);
+                    }
+                }
+                newList.replaceAll(String::trim);
+            }
+        });
+    }
+
+    @Override
+    public void handleTriggerPress(boolean pressed) {
+        if (pressed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    spinner.setVisibility(View.VISIBLE);
+                }
+            });
+            rfidHandler.performInventory();
+        } else {
+            triggerReleaseEventRecieved();
+        }
+    }
+    public void triggerReleaseEventRecieved() {
+        rfidHandler.stopInventory();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //instantiate custom adapter
+                // Create a new ArrayList
+                ArrayList<String> finalList = new ArrayList<String>();
+
+                // Traverse through the first list
+                for (String element : newList) {
+
+                    // If this element is not present in newList
+                    // then add it
+                    if (!finalList.contains(element)) {
+
+                        finalList.add(element);
+                    }
+                }
+                CustomizedListViewBulkUpdate adapter = new CustomizedListViewBulkUpdate(finalList, BulkUpdateActivity.this);
+                codeList.setAdapter(adapter);
+                spinner.setVisibility(View.GONE);
+                Log.e("FINALLLLLL",finalList.toString());
+            }
+        });
+    }
+
 }
