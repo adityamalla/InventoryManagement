ab5020e16d5e516a481c16ef22f8c6fc1e325545
adityamalla <aditya.m.v.s.s@gmail.com>
2023-05-31 22:44:48 +0530

    commit

--- app/src/main/java/com/safetystratus/inventorymanagement/ApiConstants.java
+++ app/src/main/java/com/safetystratus/inventorymanagement/ApiConstants.java
@@ -1,7 +1,7 @@
 package com.safetystratus.inventorymanagement;
 
 public class ApiConstants {
-    public static final String signInUrl = "https://devservices.labcliq.com/rest/19/mcms/login";
+    public static final String signInUrl = "https://services.labcliq.com/rest/19/mcms/login";
     public static final String downloadDbUrl = "/rest/19/mcms/downloadcms/all?site_id=%1$s&user_id=%2$s&token=%3$s";
     public static final String accessTokenUrl = "/rest/19/sync/check_access";
     public static final String downloadRoomInvDbUrl = "/rest/19/mcms/downloadcmsroomsinv/roomsandinv?site_id=%1$s&user_id=%2$s&token=%3$s&facil_id=%4$s";
